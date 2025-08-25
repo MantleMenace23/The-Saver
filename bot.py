372f45ff933e95a73ca8e52fe603fa57da743539
MantleMenace23 <connor.l.seiple@gmail.com>
2025-08-24 20:31:16 -0400

    color

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 # ========== CONFIG ==========
 ROLE_NAME = "Lesser Minded Goober"  # Role name to create and assign
-ROLE_COLOR= "137736"
+ROLE_COLOR= "0x021918"
 OWNER_USERNAME = "tech_boy1"
 OWNER_ID = 1273056960996184126
 INVITE_EXPIRY = 86400  # seconds (24h)
